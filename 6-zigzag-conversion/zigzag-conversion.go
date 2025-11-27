5a742bb722d6ae63c0ca4ffb830f6d8f41e5385c
Danila <105809690+ChernovDanila@users.noreply.github.com>
2025-11-27 20:40:21 +0300

    Time: 10 ms (35.40%) | Memory: 9.3 MB (38.64%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.go
@@ -0,0 +1,31 @@
+func convert(s string, numRows int) string {
+    
+    if numRows == 1 || numRows >= len(s) {
+        return s
+    }
+    
+    rows := make([]string, numRows)
+    currentRow := 0
+    goingDown := false
+    
+    for _, char := range s {
+        rows[currentRow] += string(char)
+        
+        if currentRow == 0 || currentRow == numRows-1 {
+            goingDown = !goingDown
+        }
+        
+        if goingDown {
+            currentRow++
+        } else {
+            currentRow--
+        }
+    }
+    
+    result := ""
+    for _, row := range rows {
+        result += row
+    }
+    return result
+
+}
