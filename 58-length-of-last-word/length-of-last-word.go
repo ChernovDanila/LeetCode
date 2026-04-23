7bfc1b32b8cc8b000f2478ddd0f111cdf9636e9c
Danila <105809690+ChernovDanila@users.noreply.github.com>
2026-04-23 17:33:07 +0300

    Time: 0 ms (100.00%) | Memory: 4.1 MB (35.15%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.go
@@ -0,0 +1,9 @@
+func lengthOfLastWord(s string) int {
+    s = strings.TrimRight(s, " ") 
+    for i:= len(s)-1; i>=0; i--{
+        if s[i]== ' '{
+            return len(s)-1-i
+        }
+    }
+    return len(s)   
+}
